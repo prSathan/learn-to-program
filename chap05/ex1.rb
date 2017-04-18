8d947474b60957995459968bec396a77465b244a
Prabu Sathan <prabu.sathan@gmail.com>
2017-04-19 00:25:10 +0100

    Chapter 5 Exercises

---
+++ chap05/ex1.rb
@@ -0,0 +1,11 @@
+print "What's your first name? "
+
+first_name = gets.chomp
+
+print "What's your middle name? "
+middle_name = gets.chomp
+
+print "What's your last name? "
+last_name = gets.chomp
+
+puts "Hi #{first_name} #{middle_name} #{last_name}"
---
+++ chap05/ex2.rb
@@ -0,0 +1,5 @@
+print "Please enter your favorite number: "
+
+number = gets.chomp.to_i
+
+puts "This is a bigger and better number to have as a favorite: #{number + 1}"
